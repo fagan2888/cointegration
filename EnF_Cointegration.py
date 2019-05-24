20b1d3f8dd32dad9cf329f74a241fbb13347b34c
Richard <r50206v@gmail.com>
2019-05-24 18:33:22 +0800

    [FEATURE] two main strategies based on co-integrating concept

---
+++ EnF_Cointegration.py
@@ -0,0 +1,68 @@
+import bt
+import sys
+import glob
+import pickle
+import datetime
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+
+'''
+另外一種就是等市值去製造新商品
+電子*4000*2-金融*1000*3+常數(讓數值變正)
+然後無腦套短、長MA穿越就行XDD
+'''
+
+
+ew = 2 
+fw = 3
+maLong = 20
+maShort = 5
+freq = '1D'
+start_date = '1990-01-01'
+
+
+dfList = []
+for file in glob.glob(sys.path[0] + '/*.csv'):
+    tmp = pd.read_csv(file, index_col=0)[['Value_Close']]
+    tmp.rename(columns={'Value_Close': file.split('-')[0].split('/')[-1]}, inplace=True)
+    tmp.index = pd.to_datetime(tmp.index)
+    dfList.append(tmp)
+df = pd.concat(dfList, axis=1)
+df = df.resample(freq).last()
+df.fillna(method='ffill', inplace=True)
+print(df.tail(3))
+
+
+df['NewIndex'] = df['EXF1']*4000*ew - df['FXF1']*1000*fw
+constant = abs(df['NewIndex'].min()) + df['NewIndex'].loc[df['NewIndex'] > 0].mean()
+# to make sure the index is always positive
+df['NewIndex'] = df['NewIndex'] + constant
+df['NI_maShort'] = df['NewIndex'].rolling(maShort).mean()
+df['NI_maLong'] = df['NewIndex'].rolling(maLong).mean()
+weights = df.copy(deep=True)
+weights.dropna(inplace=True)
+
+
+weights['FXF1'] = weights['NI_maShort'] > weights['NI_maLong']
+weights['FXF1'] = weights['FXF1'].replace({True: -1*fw, False: fw})
+weights['EXF1'] = weights['NI_maShort'] > weights['NI_maLong']
+weights['EXF1'] = weights['EXF1'].replace({True: ew, False: -1*ew})
+weights = weights[['FXF1', 'EXF1']]
+
+
+s = bt.Strategy('EnF', [
+    bt.algos.SelectAll(),
+    bt.algos.WeighTarget(weights=weights),
+    bt.algos.Rebalance()
+])
+t = bt.Backtest(s, df[['FXF1', 'EXF1']].loc[df.index > start_date])
+res = bt.run(t)
+print('\n', res.display())
+
+
+file_dst = sys.path[0] + '/result/EnF-%s.pkl' % datetime.datetime.now()
+with open(file_dst, 'wb') as f:
+    pickle.dump(res, f, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ ExF_Cointegration.py
@@ -0,0 +1,69 @@
+import bt
+import sys
+import glob
+import pickle
+import datetime
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+
+'''
+基本上金電價差應該是低相關  拿來做發散
+所以會把電子/金融當作指標
+然後隨便取個適合的MA
+
+發現最新值>MA，開始向上發散的時候
+多2電子 空3金融
+
+反之最新值<MA，開始向下發散的時候
+空2電子  多3金融
+'''
+
+
+ew = 2 
+fw = 3
+freq = '1D'
+maPeriod = 20
+start_date = '1990-01-01'
+
+
+dfList = []
+for file in glob.glob(sys.path[0] + '/*.csv'):
+    tmp = pd.read_csv(file, index_col=0)[['Value_Close']]
+    tmp.rename(columns={'Value_Close': file.split('-')[0].split('/')[-1]}, inplace=True)
+    tmp.index = pd.to_datetime(tmp.index)
+    dfList.append(tmp)
+df = pd.concat(dfList, axis=1)
+df = df.resample(freq).last()
+df.fillna(method='ffill', inplace=True)
+print(df.tail(3))
+
+
+df['Ind'] = df['EXF1'] / df['FXF1']
+df['Ind_SMA'] = df['Ind'].rolling(maPeriod).mean()
+weights = df.copy(deep=True)
+weights.dropna(inplace=True)
+
+
+weights['FXF1'] = weights['Ind'] > weights['Ind_SMA']
+weights['FXF1'] = weights['FXF1'].replace({True: -1*fw, False: fw})
+weights['EXF1'] = weights['Ind'] > weights['Ind_SMA']
+weights['EXF1'] = weights['EXF1'].replace({True: ew, False: -1*ew})
+weights.drop(['Ind', 'Ind_SMA'], axis=1, inplace=True)
+
+
+s = bt.Strategy('ExF', [
+    bt.algos.SelectAll(),
+    bt.algos.WeighTarget(weights=weights),
+    bt.algos.Rebalance()
+])
+t = bt.Backtest(s, df[['FXF1', 'EXF1']].loc[df.index > start_date])
+res = bt.run(t)
+print('\n', res.display())
+
+
+file_dst = sys.path[0] + '/result/ExF-%s.pkl' % datetime.datetime.now()
+with open(file_dst, 'wb') as f:
+    pickle.dump(res, f, protocol=pickle.HIGHEST_PROTOCOL)
